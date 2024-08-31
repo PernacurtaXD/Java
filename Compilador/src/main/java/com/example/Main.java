aa9047f544c1ff29b48ede34bccaccb60054ec4c
PernacurtaXD <168147492+PernacurtaXD@users.noreply.github.com>
2024-08-30 21:49:30 -0300

    Add files via upload

---
+++ Compilador/src/main/java/com/example/Main.java
@@ -0,0 +1,14 @@
+package com.example;
+//O compilador e o bytecode
+public class Main {
+    public static void main(String[] args) {
+        //Declaranado e inicializando variável
+        int idade = 28;
+
+        System.out.println("Eu tenho " + idade + " anos");
+
+
+        //javac Programa.java -> Programa.class -> java Programa  
+        //código fonte(Programa.java) -> bytecode(Programa.class) -> executável
+    }
+}
