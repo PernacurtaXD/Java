9998bb542e83e726701c1b2a8e7984809bf7ca98
PernacurtaXD <168147492+PernacurtaXD@users.noreply.github.com>
2025-02-11 17:29:42 +0000

    Java

---
+++ Maeveproject/app.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner escrever = new Scanner(System.in);
+        int idade;
+        String nome;
+    
+        System.out.println("Digite seu nome:");
+        nome = escrever.nextLine();
+
+
+
+    }
+
+
+}
