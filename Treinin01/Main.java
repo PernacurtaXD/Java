0327e56a492eeb7a5cf83fafd908474d30e6ea64
PernacurtaXD <168147492+PernacurtaXD@users.noreply.github.com>
2025-02-19 19:20:43 +0000

    Treinamento

---
+++ Treinin01/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+    Cliente cliente = new Cliente("Luis", "123.456.78-00", 19);
+
+
+    System.out.println(cliente.toString());
+    }
+}
